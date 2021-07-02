aa502ab5431a6bdeb712d793e72cd8b3224d0f79
Ahmed Mubarak <a.mubarak.cs@gmail.com>
2021-07-02 04:18:36 +0200

    first adding

---
+++ back-end/practicalTaskForAhmed-1/src/main/java/com/qeema/ahmedTest/practicalTask/PracticalTaskForAhmed1Application.java
@@ -0,0 +1,13 @@
+package com.qeema.ahmedTest.practicalTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTaskForAhmed1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTaskForAhmed1Application.class, args);
+	}
+
+}
---
+++ back-end/practicalTaskForAhmed-1/src/test/java/com/qeema/ahmedTest/practicalTask/PracticalTaskForAhmed1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qeema.ahmedTest.practicalTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTaskForAhmed1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
